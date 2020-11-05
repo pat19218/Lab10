3511e3bf8ba49ed43440d2c11afb9489be6565bd
pat19218 <68085020+pat19218@users.noreply.github.com>
2020-11-05 15:31:35 -0600

    eins
    por iniciar el modulo que los unifica

--- Paquete.v
+++ Paquete.v
@@ -1,6 +1,6 @@
 //couenter
 module counter #(parameter N=12)
-		(input logic clk, 
+		(input logic clk,
 		 input logic reset,
 		 input logic load,
 		 input logic enabled,
@@ -18,9 +18,9 @@ always@(posedge clk, posedge reset)
 endmodule
 
 //ROM
-module ROM(input wire [11:0]addr, output logic [7:0]data);
+module memoria_ROM (input wire [11:0]addres, output wire [7:0]data);
 
-reg [7:0]mem[0:4095];	
+	reg [7:0]mem[0:4095];
 
 initial begin
 	$readmemb("memoria.list",mem);
@@ -32,68 +32,24 @@ endmodule
 
 
 //f.f.D
-module fetch(input clk, input reset, 
-	     input ena, 
-	     input [7:0]d, 
-	     output logic [3:0]instruccion,
-	     output logic [3:0]operando );
+module fetch(input clk, input reset,
+	     input ena,
+	     input [7:0]d,
+	     output reg [3:0]instruccion,
+	     output reg [3:0]operando);
 
 
-	always @ (posedge clk, posedge reset)
+	always @ (posedge clk, posedge reset)begin
+
 		if (reset)
-			operando <= 4'b0;
-			instruccion <= 4'b0;
+			begin
+				operando <= 4'b0;
+				instruccion <= 4'b0;
+			end
 		else if (ena)
-			instruccion <= d[7:4];
-			operando <= d[3:0];			
+			begin
+				instruccion <= d[7:4];
+				operando <= d[3:0];
+			end
+	end
 endmodule
-
-//preparacion de control
-module preparacion(input clk, input reset, 
-		   input logic load_counter, input logic ena_load_counter,
-		   input logic enabled_ct, input logic enabled_fetch,
-		   input [11:0]in_dato,
-		   output logic [7:0]program_byte, output logic [3:0]intsr, output logic [3:0]oprnd);
-
-	
-	output wire [11:0]out_counter;
-	
-	counter co1(.clk(clk), .reset(reset), 
-		    .load(ena_load_counter), .enabled(enabled_ct),
-		    .d(in_dato), 
-		    .q(out_counter) );
-
-
-
-	
-	wire [7:0]out_ROM;
-
-	ROM r1(.addr(out_counter), .data(out_ROM) );
-	
-
-
-	output wire [3:0]fetch_instruccion;
-	output wire [3:0]fetch_operando;
-
-	fetch ftc(.clk(clk), .reset(reset),
-		  .ena(enabled_fetch),
-		  .d(out_ROM),
-		  .instruccion(fetch_intruccion), .operando(fetch_operando) );
-
-assign program_byte = out_ROM;
-assign intsr = fetch_intruccion;
-assign oprnd = fetch_operando;
-	
-
-endmodule
-
-
-
-
-
-
-
-
-
-
-
